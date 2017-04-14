4c877585446603553158d700ea75839ca6b553ff
Arthur Lui <luiarthur@gmail.com>
2017-04-14 09:20:59 -0700

    yes

--- demo/demo.R
+++ demo/demo.R
@@ -1,1 +1,17 @@
+# Install the library if needed. This is done only once.
+# devtools::install_github('luiarthur/genericMCMC')
 
+Rcpp::sourceCpp("fit.cpp")
+
+set.seed(1)
+n <- 10000
+p <- 0.73
+y <- rbinom(n,1,p)
+
+out <- fit(y,.02,.5,.5,2000,1000)
+acceptance_rate <- length(unique(out)) / length(out)
+
+# Plot posterior
+hist(out,fg='grey',col='grey',border='white',
+     main=paste0('Truth = ',p))
+abline(v=mean(out),col='red',lwd=2)
--- demo/fit.cpp
+++ demo/fit.cpp
@@ -1,15 +1,19 @@
 // [[Rcpp::depends(genericMCMC)]]
 
-#include <genericMCMC.h>
+#include<genericMCMC.h>
+#include<functional>
 
 using namespace Rcpp;
+
+// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
+// [[Rcpp::plugins("cpp11")]]
 
 struct State {
   double p;
 };
 
 //[[Rcpp::export]]
-NumericVector fit(NumericVector x, int cs, double a, double b, int B, int burn) {
+NumericVector fit(NumericVector x, double cs, double a, double b, int B, int burn) {
 
   const double sumx = sum(x);
   const int n = x.size();
@@ -18,7 +22,7 @@ NumericVector fit(NumericVector x, int cs, double a, double b, int B, int burn) 
   auto update = [&](State& s_old, State& s_new) {
     // log prior
     auto lp = [&](double p) {
-      double out = 1E-10;
+      double out = -INFINITY;
       if (p > 0 && p < 1) {
         out  = (a-1)*log(p) + (b-1)*log(1-p);
       }
@@ -26,19 +30,21 @@ NumericVector fit(NumericVector x, int cs, double a, double b, int B, int burn) 
     };
     // log like
     auto ll = [&](double p) {
-      double out = 1E-10;
+      double out = -INFINITY;
       if (p > 0 && p < 1) {
         out = sumx*log(p) + (n-sumx)*log(1-p);
       }
-      return out
+      return out;
     };
 
-    s_new.p = genericMCMC::metropolis(s_old.p, ll, lp, cs);
-  }
+    s_new.p = metropolis(s_old.p, ll, lp, cs);
+  };
 
-  auto samps = genericMCMC::gibbs<State>(init, update, B, burn, 0);
+  auto init = State{ 0.5 };
+  auto samps = gibbs<State>(init, update, B, burn, 0);
+
   for (int i=0; i<B; i++) {
-    out[i] samps[i].p;
+    out[i] = samps[i].p;
   }
 
   return out;
